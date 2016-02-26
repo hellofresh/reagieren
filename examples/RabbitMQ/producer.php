0b0796f6833daac87cc90ac8d88f88bca1fb1d19
Kieran Patel <me@kieranajp.co.uk>
2016-02-26 17:39:30 +0100

    Fixed Rabbit example connection details to match each other

--- examples/RabbitMQ/producer.php
+++ examples/RabbitMQ/producer.php
@@ -4,5 +4,5 @@ require_once '../vendor/autoload.php';
 
 use HelloFresh\Reagieren\MessageBroker\RabbitMQ\PHPAmqp\PHPAmqpProducerAdapter as Producer;
 
-(new Producer('rabbitmq'))->produce('example', 'Hello world this is a message');
+(new Producer('127.0.0.1', 5672, 'guest', 'guest'))->produce('example', 'Hello world this is a message');
 echo 'Message sent!', PHP_EOL;
