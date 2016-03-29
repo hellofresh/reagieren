6999770c10c90f961b92bb29acc3a540db2ae666
Ítalo Lelis de Vietro <italolelis@gmail.com>
2016-03-29 10:49:05 +0200

    Fixing tests

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -6,14 +6,16 @@ class MessageTest extends PHPUnit_Framework_TestCase
 {
     public function testMessageTimestampIsDateObject()
     {
-        $message = new Message('something', 'now', 'utf-8');
+        $date = new \DateTime();
+        $message = new Message('something', $date, 'utf-8');
 
-        $this->assertInstanceOf(\DateTimeImmutable::class, $message->getTimestamp());
+        $this->assertInstanceOf(\DateTime::class, $message->getTimestamp());
     }
 
     public function testCastingMessageToStringReturnsPayload()
     {
-        $message = new Message('something', 'now', 'utf-8');
+        $date = new \DateTime();
+        $message = new Message('something', $date, 'utf-8');
 
         $this->assertEquals('something', (string) $message);
     }
